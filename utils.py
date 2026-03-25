d5f43d93bbcecf42243af4d539aa3c20dcfdfaa9
Ali Raza <aliraza9052205@gmail.com>
2026-03-25 21:48:03 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 #from langchain.text_splitter import CharacterTextSplitter
 from langchain_text_splitters import CharacterTextSplitter
-from langchain.document_loaders import TextLoader
+#from langchain.document_loaders import TextLoader
+from langchain_community.document_loaders import TextLoader
 from langchain.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
