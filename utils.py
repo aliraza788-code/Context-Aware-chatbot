2e0388aa00a56bfbb9fee8e24a87390b1f979c66
Ali Raza <aliraza9052205@gmail.com>
2026-03-25 21:51:26 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
 from langchain_groq import ChatGroq
-from langchain.chains import RetrievalQA
+from langchain_community.chains import RetrievalQA
 
 
 def load_data():
